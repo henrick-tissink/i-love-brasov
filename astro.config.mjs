37d25f0fa56a01b1742fc986c62d3bfa93f7c077
Henrick Tissink <hltissink@gmail.com>
2024-11-19 19:53:52 +0200

    I love brasov

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+
+import react from "@astrojs/react";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://ilovebrasov.com",
+  integrations: [react(), tailwind(), sitemap()],
+});
